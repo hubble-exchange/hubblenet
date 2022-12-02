45eda973d7b20861f7c82dc289849c9854c97fe8
Shubham Goyal <sgshubham@gmail.com>
2022-12-02 11:39:36 +0530

    Review changes

--- contract-examples/scripts/deployERC20NativeMinter.ts
+++ contract-examples/scripts/deployERC20NativeMinter.ts
@@ -9,15 +9,6 @@ const main = async (): Promise<any> => {
   const token: Contract = await Token.deploy(5000)
 
   await token.deployed()
-  console.log(`Token deployed to: ${token.address}`)
-  const name: string = await token.name()
-  console.log(`Name: ${name}`)
-
-  const symbol: string = await token.symbol()
-  console.log(`Symbol: ${symbol}`)
-
-  const decimals: string = await token.decimals()
-  console.log(`Decimals: ${decimals}`)
 }
 
 main()
