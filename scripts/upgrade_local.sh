d9cfb2492f577ef9b0338b1892fe380b39faa660
Shubham Goyal <sgshubham@gmail.com>
2023-04-03 10:49:27 +0530

    Fix local run scripts

--- scripts/upgrade_local.sh
+++ scripts/upgrade_local.sh
@@ -9,7 +9,6 @@ avalanche network stop --snapshot-name snap1
 avalanche subnet upgrade vm hubblenet --binary custom_evm.bin --local
 
 # utse tee to keep showing outut while storing in a var
-avalanche network start --avalanchego-version v1.9.14 --snapshot-name snap1 --config .avalanche-cli.json
-# OUTPUT=$(avalanche network start --avalanchego-version v1.9.14 --snapshot-name snap1 --config .avalanche-cli.json | tee /dev/fd/2)
+OUTPUT=$(avalanche network start --avalanchego-version v1.9.14 --snapshot-name snap1 --config .avalanche-cli.json | tee /dev/fd/2)
 
 setStatus
