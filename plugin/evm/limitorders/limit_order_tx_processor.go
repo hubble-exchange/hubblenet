577dbff5a044406e33bba7f637df0ebb5a42a868
Arpit <3612498+atvanguard@users.noreply.github.com>
2023-06-01 15:31:08 +0100

    prepare hubblenext configs

--- plugin/evm/limitorders/limit_order_tx_processor.go
+++ plugin/evm/limitorders/limit_order_tx_processor.go
@@ -138,7 +138,7 @@ func (lotp *limitOrderTxProcessor) executeLocalTx(contract common.Address, contr
 		log.Error("HexToECDSA failed", "err", err)
 		return err
 	}
-	tx := types.NewTransaction(nonce, contract, big.NewInt(0), 3000000, lotp.validatorTxFeeConfig.baseFeeEstimate, data)
+	tx := types.NewTransaction(nonce, contract, big.NewInt(0), 1000000, lotp.validatorTxFeeConfig.baseFeeEstimate, data)
 	signer := types.NewLondonSigner(lotp.backend.ChainConfig().ChainID)
 	signedTx, err := types.SignTx(tx, signer, key)
 	if err != nil {
