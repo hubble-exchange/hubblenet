28e97945ee9bc558c5933a27a1b87551ae5863b5
Arpit <3612498+atvanguard@users.noreply.github.com>
2023-06-12 00:05:41 +0100

    add tests

--- precompile/contracts/hubblebibliophile/orderbook.go
+++ precompile/contracts/hubblebibliophile/orderbook.go
@@ -1,7 +1,7 @@
 package hubblebibliophile
 
 import (
-	"fmt"
+	"errors"
 	"math/big"
 
 	"github.com/ava-labs/subnet-evm/precompile/contract"
@@ -16,60 +16,80 @@ const (
 	ORDER_INFO_SLOT           int64 = 53
 )
 
+var (
+	ErrNotLongOrder  = errors.New("OB_order_0_is_not_long")
+	ErrNotShortOrder = errors.New("OB_order_1_is_not_short")
+	ErrNotSameAMM    = errors.New("OB_orders_for_different_amms")
+	ErrNoMatch       = errors.New("OB_orders_do_not_match")
+	ErrInvalidOrder  = errors.New("OB_invalid_order")
+	ErrNotMultiple   = errors.New("OB.not_multiple")
+	ErrTooLow        = errors.New("OB_long_order_price_too_low")
+	ErrTooHigh       = errors.New("OB_short_order_price_too_high")
+)
+
 func ValidateOrdersAndDetermineFillPrice(stateDB contract.StateDB, inputStruct *ValidateOrdersAndDetermineFillPriceInput) (*ValidateOrdersAndDetermineFillPriceOutput, error) {
 	longOrder := inputStruct.Orders[0]
-	shortOrder := inputStruct.Orders[0]
+	shortOrder := inputStruct.Orders[1]
 
 	if longOrder.BaseAssetQuantity.Cmp(big.NewInt(0)) <= 0 {
-		return nil, fmt.Errorf("OB_order_0_is_not_long")
+		return nil, ErrNotLongOrder
 	}
 
 	if shortOrder.BaseAssetQuantity.Cmp(big.NewInt(0)) >= 0 {
-		return nil, fmt.Errorf("OB_order_1_is_not_short")
+		return nil, ErrNotShortOrder
 	}
 
 	if longOrder.AmmIndex.Cmp(shortOrder.AmmIndex) != 0 {
-		return nil, fmt.Errorf("OB_orders_for_different_amms")
+		return nil, ErrNotSameAMM
 	}
 
 	if longOrder.Price.Cmp(shortOrder.Price) == -1 {
-		return nil, fmt.Errorf("OB_orders_do_not_match")
+		return nil, ErrNoMatch
+	}
+
+	if getOrderStatus(stateDB, inputStruct.OrderHashes[0]) != 1 || getOrderStatus(stateDB, inputStruct.OrderHashes[1]) != 1 {
+		return nil, ErrInvalidOrder
 	}
 
 	market := getMarketAddressFromMarketID(longOrder.AmmIndex.Int64(), stateDB)
+	minSize := GetMinSizeRequirementForMarket(stateDB, longOrder.AmmIndex.Int64())
+	if new(big.Int).Mod(inputStruct.FillAmount, minSize).Cmp(big.NewInt(0)) != 0 {
+		return nil, ErrNotMultiple
+	}
+
 	oraclePrice := getUnderlyingPrice(stateDB, market)
 	spreadLimit := GetMaxOracleSpreadRatioForMarket(stateDB, longOrder.AmmIndex.Int64())
+	blockPlaced0 := getBlockPlaced(stateDB, inputStruct.OrderHashes[0])
+	blockPlaced1 := getBlockPlaced(stateDB, inputStruct.OrderHashes[1])
+
+	return determineFillPrice(oraclePrice, spreadLimit, longOrder.Price, shortOrder.Price, blockPlaced0, blockPlaced1)
+}
+
+func determineFillPrice(oraclePrice, spreadLimit, longOrderPrice, shortOrderPrice, blockPlaced0, blockPlaced1 *big.Int) (*ValidateOrdersAndDetermineFillPriceOutput, error) {
 	upperbound := divide1e6(new(big.Int).Mul(oraclePrice, new(big.Int).Add(_1e6, spreadLimit)))
 	lowerbound := big.NewInt(0)
 	if spreadLimit.Cmp(_1e6) == -1 {
 		lowerbound = divide1e6(new(big.Int).Mul(oraclePrice, new(big.Int).Sub(_1e6, spreadLimit)))
 	}
-	if longOrder.Price.Cmp(lowerbound) == -1 {
-		return nil, fmt.Errorf("OB_long_order_price_too_low")
+	if longOrderPrice.Cmp(lowerbound) == -1 {
+		return nil, ErrTooLow
 	}
-	if shortOrder.Price.Cmp(upperbound) == 1 {
-		return nil, fmt.Errorf("OB_short_order_price_too_high")
+	if shortOrderPrice.Cmp(upperbound) == 1 {
+		return nil, ErrTooHigh
 	}
 
-	// CUSTOM CODE FOR AN OUTPUT
 	output := ValidateOrdersAndDetermineFillPriceOutput{}
-
-	blockPlaced0 := getBlockPlaced(stateDB, inputStruct.OrderHashes[0])
-	blockPlaced1 := getBlockPlaced(stateDB, inputStruct.OrderHashes[1])
-
 	if blockPlaced0.Cmp(blockPlaced1) == -1 {
 		// long order is the maker order
-		output.FillPrice = utils.BigIntMin(longOrder.Price, upperbound)
+		output.FillPrice = utils.BigIntMin(longOrderPrice, upperbound)
 		output.Mode0 = 1 // Mode0 corresponds to the long order and `1` is maker
 		output.Mode1 = 0 // Mode1 corresponds to the short order and `0` is taker
 	} else { // if long order is placed after short order or in the same block as short
 		// short order is the maker order
-		output.FillPrice = utils.BigIntMax(shortOrder.Price, lowerbound)
+		output.FillPrice = utils.BigIntMax(shortOrderPrice, lowerbound)
 		output.Mode0 = 0 // Mode0 corresponds to the long order and `0` is taker
 		output.Mode1 = 1 // Mode1 corresponds to the short order and `1` is maker
 	}
-
-	// CUSTOM CODE ENDS HERE
 	return &output, nil
 }
 
@@ -78,9 +98,9 @@ func getBlockPlaced(stateDB contract.StateDB, orderHash [32]byte) *big.Int {
 	return new(big.Int).SetBytes(stateDB.GetState(common.HexToAddress(ORDERBOOK_GENESIS_ADDRESS), common.BigToHash(orderInfo)).Bytes())
 }
 
-func getOrderStatus(stateDB contract.StateDB, orderHash [32]byte) *big.Int {
+func getOrderStatus(stateDB contract.StateDB, orderHash [32]byte) int64 {
 	orderInfo := orderInfoMappingStorageSlot(orderHash)
-	return new(big.Int).SetBytes(stateDB.GetState(common.HexToAddress(ORDERBOOK_GENESIS_ADDRESS), common.BigToHash(new(big.Int).Add(orderInfo, big.NewInt(3)))).Bytes())
+	return new(big.Int).SetBytes(stateDB.GetState(common.HexToAddress(ORDERBOOK_GENESIS_ADDRESS), common.BigToHash(new(big.Int).Add(orderInfo, big.NewInt(3)))).Bytes()).Int64()
 }
 
 func orderInfoMappingStorageSlot(orderHash [32]byte) *big.Int {
