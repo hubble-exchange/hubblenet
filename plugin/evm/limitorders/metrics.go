7ff34a47ebd8126473ca003213d86a433472a285
Shubham Goyal <sgshubham@gmail.com>
2023-06-23 15:56:14 +0530

    Use resetting sampler

--- plugin/evm/limitorders/metrics.go
+++ plugin/evm/limitorders/metrics.go
@@ -5,10 +5,10 @@ import (
 )
 
 var (
-	transactionsPerBlockHistogram = metrics.NewRegisteredHistogram("transactions/rate", nil, metrics.NewExpDecaySample(1028, 0.015))
+	transactionsPerBlockHistogram = metrics.NewRegisteredHistogram("transactions/rate", nil, metrics.ResettingSample(metrics.NewExpDecaySample(1028, 0.015)))
 
-	gasUsedPerBlockHistogram      = metrics.NewRegisteredHistogram("gas_used_per_block", nil, metrics.NewExpDecaySample(1028, 0.015))
-	blockGasCostPerBlockHistogram = metrics.NewRegisteredHistogram("block_gas_cost", nil, metrics.NewExpDecaySample(1028, 0.015))
+	gasUsedPerBlockHistogram      = metrics.NewRegisteredHistogram("gas_used_per_block", nil, metrics.ResettingSample(metrics.NewExpDecaySample(1028, 0.015)))
+	blockGasCostPerBlockHistogram = metrics.NewRegisteredHistogram("block_gas_cost", nil, metrics.ResettingSample(metrics.NewExpDecaySample(1028, 0.015)))
 
 	// only valid for OrderBook transactions send by this validator
 	orderBookTransactionsSuccessTotalCounter = metrics.NewRegisteredCounter("orderbooktxs/total/success", nil)
