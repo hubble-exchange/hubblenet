de19c58f5b02fa9d2a8147b072cdec891cd21263
atvanguard <3612498+atvanguard@users.noreply.github.com>
2023-06-26 13:31:52 +0100

    comment out FixBuggySnapshot

--- plugin/evm/limit_order.go
+++ plugin/evm/limit_order.go
@@ -111,7 +111,7 @@ func (lop *limitOrderProcesser) ListenAndProcessTransactions() {
 			toBlock = utils.BigIntMin(lastAccepted, big.NewInt(0).Add(fromBlock, big.NewInt(10000)))
 		}
 		lop.memoryDb.Accept(lastAccepted.Uint64()) // will delete stale orders from the memorydb
-		lop.FixBuggySnapshot()                     // not required any more
+		// lop.FixBuggySnapshot()                     // not required any more
 	}
 
 	lop.mu.Unlock()
