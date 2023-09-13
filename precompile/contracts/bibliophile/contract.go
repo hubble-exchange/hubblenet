64f698171c2706bcdfca66eb8a20c2fcea40c569
Shubham <sgshubham@gmail.com>
2023-09-13 11:52:18 +0530

    Remove backward compatible changes/upgrades (#109)
    * Remove backward compatible changes/upgrades
    
    * More changes
    
    * Review fixes and more breaking changes
    
    * Rename to BaseOrder

--- precompile/contracts/bibliophile/contract.go
+++ precompile/contracts/bibliophile/contract.go
@@ -134,7 +134,7 @@ func getNotionalPositionAndMargin(accessibleState contract.AccessibleState, call
 	}
 
 	// CUSTOM CODE STARTS HERE
-	output := GetNotionalPositionAndMargin(accessibleState.GetStateDB(), &inputStruct, new(big.Int).SetUint64(accessibleState.GetBlockContext().Timestamp()))
+	output := GetNotionalPositionAndMargin(accessibleState.GetStateDB(), &inputStruct)
 	packedOutput, err := PackGetNotionalPositionAndMarginOutput(output)
 	if err != nil {
 		return nil, remainingGas, err
