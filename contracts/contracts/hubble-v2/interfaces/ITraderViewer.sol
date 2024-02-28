feeb50451f4e4ab07a4d7a26f179c1870629dab4
atvanguard <3612498+atvanguard@users.noreply.github.com>
2024-02-29 00:04:28 +0530

    generate trader view precompile

---
+++ contracts/contracts/hubble-v2/interfaces/ITraderViewer.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { IClearingHouse } from "./IClearingHouse.sol";
+
+interface ITraderViewer {
+    function getNotionalPositionAndMargin(address trader, bool includeFundingPayments, IClearingHouse.Mode mode) external view returns(uint256 notionalPosition, int256 margin, uint256 requiredMargin);
+
+    function getTraderDataForMarket(address trader, uint256 ammIndex, IClearingHouse.Mode mode) external view returns(
+        bool isIsolated, uint256 notionalPosition, int256 unrealizedPnl, uint256 requiredMargin, int256 pendingFunding
+    );
+
+    function getCrossMarginAccountData(address trader, IClearingHouse.Mode mode)
+        external
+        view
+        returns(uint256 notionalPosition, uint256 requiredMargin, int256 unrealizedPnl, int256 pendingFunding);
+
+    function getTotalFundingForCrossMarginPositions(address trader) external view returns(int256 totalFunding);
+
+}
